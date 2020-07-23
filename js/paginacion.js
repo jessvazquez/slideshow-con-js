fddf970298e3fee8fbace68c77a982658b14a0c1
jessvazquez <31674161+jessvazquez@users.noreply.github.com>
2020-07-23 00:31:25 -0500

    Funcion de Slide por medio de flechas

--- js/paginacion.js
+++ js/paginacion.js
@@ -3,10 +3,17 @@ var p = {
 
     // Seleccionamos todos los li del id paginacion
     paginacion: document.querySelectorAll("#paginacion li"),
+
     item: 0,
     // Capturamos el ul del id slide.
-    cajaSlide: document.querySelector("#slide ul")
+    cajaSlide: document.querySelector("#slide ul"),
 
+    flechaRetroceder: document.querySelector("#slide #retroceder"),
+
+    flechaAvanzar: document.querySelector("#slide #avanzar"),
+
+    // Seleccionamos la cantidad de imagenes que tenemos.
+    imgSlide: document.querySelectorAll("#slide ul li")
 
 }
 
@@ -21,6 +28,9 @@ var m = {
             p.paginacion[i].addEventListener("click", m.paginacionSlide);
         }
 
+        //Listener de Flechas.
+        p.flechaAvanzar.addEventListener("click", m.avanzar);
+        p.flechaRetroceder.addEventListener("click", m.retroceder);
 
     },
 
@@ -37,8 +47,32 @@ var m = {
         m.movimientoSlide(p.item);
     },
 
+    avanzar: function() {
+
+        if (p.item == p.imgSlide.length - 1) {
+            p.item = 0;
+        } else {
+            p.item++;
+        }
+
+        console.log(p.imgSlide.length);
+
+        m.movimientoSlide(p.item);
+
+    },
+
+    retroceder: function(item) {
+        if (p.item == 0) {
+            p.item = p.imgSlide.length - 1;
+        } else {
+            p.item--;
+        }
+        m.movimientoSlide(p.item);
+    },
+
     movimientoSlide: function(item) {
         // Para obtener el desplazamiento de la img.
+
         console.log(-(item * 100) + "%");
 
         // Cambiamos el porcentaje de la propiedad
@@ -54,13 +88,10 @@ var m = {
         // Para que cambie la intensidad de la opacidad del item seleccionado.
         p.paginacion[item].style.opacity = 1;
 
-        //Que la img que se desplaza tenga efecto de transition.
+        //Que la img que se desplaza tenga efecto de transition a la propiedad left del ul.
         p.cajaSlide.style.transition = ".7s left ease-in-out";
 
     }
-
-
-
 }
 
 // cargamos el metodo al inicio de la pagina.
